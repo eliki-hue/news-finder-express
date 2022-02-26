516b57020d4141f3529397c364c769f668628583
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-27 00:22:37 +0300

    created channel class and channel_test class

---
+++ app/channel_test.py
@@ -0,0 +1,6 @@
+import unittest
+from app.models import channel
+
+Channel = channel.Channel
+
+class ChannelTest(unittest.TestCase):
---
+++ app/models/channel.py
@@ -0,0 +1,5 @@
+from msilib.schema import Class
+class Channel:
+    '''
+    Channel class to define channel object
+    '''
