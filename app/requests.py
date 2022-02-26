2b4f32660272a58955e4b82654e3640137bd8309
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-27 01:25:54 +0300

    defined process function that creats a channel object from url request results

--- app/requests.py
+++ app/requests.py
@@ -21,5 +21,26 @@ def get_channels(search):
         get_news_response = json.loads(get_news_data)
         news_result = None
 
-        news_result = process(get_news_response
-        )
+        news_result = process(get_news_response)
+        return news_result
+        
+
+def process(channel):
+    '''
+    function that processes channel results
+    '''
+    channel_info = []
+    for item in channel:
+        id = channel.get('id')
+        name = channel.get('name')
+        description = channel.get('description')
+        url = channel.get('url')
+        category = channel.get('category')
+        country = channel.get('country')
+
+        channel_object = Channel(id, name, description, url, category, country)
+        channel_info.append(channel_object)
+
+    return channel_info
+    
+
