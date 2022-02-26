4cb72ac5627b40287c21bef8f73ae33228009e8c
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-27 00:47:36 +0300

    added Channel class properties and challel_test class properties

--- app/channel_test.py
+++ app/channel_test.py
@@ -1,6 +1,22 @@
 import unittest
-from app.models import channel
+from models import channel
 
 Channel = channel.Channel
 
 class ChannelTest(unittest.TestCase):
+     '''
+    Test Class to test the behaviour of the Channel class
+    '''
+
+     def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_channel = Channel(1234,'cnn','an international news source','https://cnn.news','politics','us')
+
+     def test_instance(self):
+        self.assertTrue(isinstance(self.new_channel,Channel))
+
+
+if __name__ == '__main__':
+    unittest.main()
--- app/models/channel.py
+++ app/models/channel.py
@@ -1,5 +1,11 @@
-from msilib.schema import Class
+
 class Channel:
     '''
     Channel class to define channel object
     '''
+    def __init__(self, id, name, description, url, category, country):
+        self.id = id
+        self.name = name
+        self.description =description
+        self.url =url
+        self.category = category
