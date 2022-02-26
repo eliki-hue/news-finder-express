e75165971ab81060a99015eefacb53dfe5c86e30
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-27 01:34:20 +0300

    added channel vded code to display channels in index.html

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from gettext import install
 from flask import render_template
 from app import app
+from app.requests import get_channels
 
 
 
@@ -11,4 +12,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    # getting channels
+    channels = get_channels('sources')
+    print(channels)
+    title = 'Welcome to this website that displays all the news channels'
+    return render_template('index.html', title =title, sources= channels)
