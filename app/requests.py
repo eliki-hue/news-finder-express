a8f699592199b293c3127276cf44bc00ffe24d99
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-27 00:53:54 +0300

    accessed the bccessed the news base url in request.py

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,12 @@
 from app import app
+import urllib.request, json
+from models import channel
+
+Channel = channel.Channel
 
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#getting the news base url
+base_url = app.config['NEWS_API_BASE_URL']
