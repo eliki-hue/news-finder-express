d7a291a2cde5d21dee6ccd8abbe6d4db894960ea
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-28 13:25:18 +0300

    initiated Manager to serve the app

---
+++ manage.py
@@ -0,0 +1,9 @@
+from flask_script import Manager, Server
+from app import creat_app
+app = creat_app('development')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+if __name__=='__main__':
+    manager.run()
