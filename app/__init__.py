66f939c2193a2ae02cdc28952f00caa87b9a221a
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-02-28 13:21:27 +0300

    added creat_app function that initiates the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,26 @@
+from ensurepip import bootstrap
 from flask import Flask
 from config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def creat_app(config_name):
+
+    app =Flask(__name__)
+    from app import views
+
+    app.config.from_object(config_options[config_name])
+
+    bootstrap.init_app(app)
 
 
-app =Flask(__name__, instance_relative_config=True)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Setting up configuration
+    # app.config.from_object(DevConfig)
+    # app.config.from_pyfile('config.py')
+    return app
 
 
-from app import views
+    
