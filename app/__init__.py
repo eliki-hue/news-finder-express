6409fea30153dd543f307691eb648674c167ccb8
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-03-01 15:12:09 +0300

    added bootstrap and navbar

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from flask_bootstrap import Bootstrap
 
 app =Flask(__name__, instance_relative_config=True)
 
+Bootstrap(app)
+
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
